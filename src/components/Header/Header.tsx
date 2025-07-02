90eea3f415478182f01e7b4e637131f332d89d3c
Zsolt <184605676+Zsolt-qwerty@users.noreply.github.com>
2025-07-02 23:56:25 +0100

    fix icon filepath in header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,10 +1,12 @@
+import graphIcon from '/icons8-stock-market-96.png';
 import './Header.css';
 
 function Header() {
   return (
     <header>
+      test 01
       <div>
-        <img src="/icons8-stock-market-96.png" height="48" alt="Stockbook Logo" />
+        <img src={graphIcon} height="48" alt="Stockbook Logo" />
       </div>
       <h1>stockbook v0.01</h1>
     </header>
--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/stockbook/',
+  base: '/stockbook',
   plugins: [react()],
 })
