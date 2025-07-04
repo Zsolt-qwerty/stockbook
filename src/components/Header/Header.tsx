6067155a6c4fb027925389aeb97577da75851084
Zsolt <184605676+Zsolt-qwerty@users.noreply.github.com>
2025-07-04 12:55:48 +0100

    test deployment

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -3,7 +3,7 @@ import './Header.css';
 function Header() {
   return (
     <header>
-      test 04
+      test 01
       <div>
         <img src="icons8-stock-market-96.png" height="48" alt="Stockbook Logo" />
       </div>
