b22114004b291009e65a503ec60b3b23da1d027c
Zsolt <184605676+Zsolt-qwerty@users.noreply.github.com>
2025-07-05 13:10:12 +0100

    displaying first stock's price, .env setup

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,11 +1,11 @@
+import stockbookLogo from "/icons8-stock-market-96.png";
 import './Header.css';
 
 function Header() {
   return (
     <header>
-      test 01
       <div>
-        <img src="icons8-stock-market-96.png" height="48" alt="Stockbook Logo" />
+        <img src={stockbookLogo} height="48" alt="Stockbook Logo" />
       </div>
       <h1>stockbook v0.0.0</h1>
     </header>
