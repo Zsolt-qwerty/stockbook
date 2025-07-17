0ed043db9415ef47965e759ebbb229696344bdf9
Zsolt-qwerty <184605676+Zsolt-qwerty@users.noreply.github.com>
2025-07-17 16:32:40 +0100

    feat: added news section and more stock details, updated layout

--- src/components/Main/Main.tsx
+++ src/components/Main/Main.tsx
@@ -1,11 +1,27 @@
 import { useState, useEffect } from 'react';
-import { ApiClient, DefaultApi, QuoteData } from 'finnhub';
 import './Main.css';
+import {
+  ApiClient,
+  DefaultApi,
+  CompanyProfile2Data,
+  QuoteData,
+  CompanyNewsData,
+} from 'finnhub';
 import { getEnvVariableOrThrow } from '../../utils/getEnvVariableOrThrow';
 
+import StockCard from '../StockCard/StockCard';
+import InfoCard from '../InfoCard/InfoCard';
+import NewsCard from '../NewsCard/NewsCard';
+
 function Main() {
-  const [count, setCount] = useState(0);
-  const [stockData, setStockData] = useState({} as QuoteData);
+
+  // Example ticker symbols, can be changed to any valid symbols
+  const tickers: string[] = ["AAPL", "TSLA"];
+
+  const [companyProfile2Data, setCompanyProfile2Data] = useState({} as CompanyProfile2Data);
+  const [stockQuoteData, setStockQuoteData] = useState({} as QuoteData);
+  const [companyNewsData, setCompanyNewsData] = useState({} as CompanyNewsData);
+  const [ticker, setTicker] = useState(tickers[0] as string);
 
   useEffect(() => {
     // This effect runs once when the component mounts
@@ -15,18 +31,39 @@ function Main() {
     api_key.apiKey = getEnvVariableOrThrow('VITE_FINNHUB_TOKEN');
     const finnhubClient = new DefaultApi();
 
-    const ticker: string = "AAPL"; // Example ticker symbol, can be changed to any valid symbol
+    finnhubClient.companyProfile2({"symbol": ticker}, (error: Error | null, data: CompanyProfile2Data, response: unknown) => {
+      if (error) {
+        console.error('API Error:', error);
+        return;
+      }
+      console.log('Company Profile 2 data:', data);
+      setCompanyProfile2Data(data);
+      console.log('API response:', response);
+    });
+    
     finnhubClient.quote(ticker, (error: Error | null, data: QuoteData, response: unknown) => {
       if (error) {
         console.error('API Error:', error);
         return;
       }
       console.log('Quote data:', data);
-      setStockData(data);
+      setStockQuoteData(data);
       console.log('API response:', response);
     });
 
-    // Uncomment the following lines to enable WebSocket connection
+    // const yesterday = new Date(Date.now() - 86400000).toISOString().slice(0, 10);
+    // const today = new Date().toISOString().slice(0, 10);
+    finnhubClient.companyNews(ticker, "2025-07-14", "2025-07-14", (error: Error | null, data: CompanyNewsData[], response: unknown) => {
+      if (error) {
+      console.error('API Error:', error);
+      return;
+      }
+      console.log('Company News data:', data);
+      setCompanyNewsData(data[0]);
+      console.log('API response:', response);
+    });
+
+    // // Uncomment the following lines to enable WebSocket connection
     // const token = import.meta.env.VITE_FINNHUB_TOKEN;
     // if (!token) {
     //   console.error('VITE_FINNHUB_TOKEN is not set in environment variables');
@@ -57,20 +94,35 @@ function Main() {
     //   console.log('Component unmounted');
     //   socket.close();
     // };
-  }, []);
+  }, [ticker]);
 
   return (
     <main>
-      <div className="card">
-        Apple: ${stockData.c} {stockData.d} ({stockData.dp}%)
-      </div>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+      <div className="main-window">
+        <div className="left-window">
+          <StockCard
+            companyProfile2Data={companyProfile2Data}
+            stockQuoteData={stockQuoteData}
+          />
+          <StockCard
+            companyProfile2Data={companyProfile2Data}
+            stockQuoteData={stockQuoteData}
+          />
+          <StockCard
+            companyProfile2Data={companyProfile2Data}
+            stockQuoteData={stockQuoteData}
+          />
+          <button onClick={() => ticker === tickers[0] ? setTicker(tickers[1]) : setTicker(tickers[0])}>
+            {ticker === tickers[0] ? tickers[1] : tickers[0]}
+          </button>(temporary button for testing)
+        </div>
+        <div className="right-window">
+          <InfoCard
+            companyProfile2Data={companyProfile2Data}
+            stockQuoteData={stockQuoteData}
+          />
+          <NewsCard companyNewsData={companyNewsData} />
+        </div>
       </div>
     </main>
   );
---
+++ src/components/StockCard/StockCard.tsx
@@ -0,0 +1,39 @@
+import { CompanyProfile2Data, QuoteData } from "finnhub";
+import { ReactNode } from "react";
+
+type StockCardProps = {
+  companyProfile2Data: CompanyProfile2Data;
+  stockQuoteData: QuoteData;
+}
+
+function StockCard({ companyProfile2Data, stockQuoteData }: StockCardProps): ReactNode {
+
+  if (!stockQuoteData || !companyProfile2Data || !companyProfile2Data.ticker) {
+    return <div className="card stock">No stock data available</div>;
+  }
+
+  return (
+    <div className="card stock">
+      <div className="stockLogo">
+        <img src={companyProfile2Data.logo} alt="" />
+      </div>
+      <div className="left">
+        {companyProfile2Data.name}
+      </div>
+      <div className="right">
+        {companyProfile2Data.currency} {stockQuoteData.c}
+      </div>
+      <div className="left gray">
+        {companyProfile2Data.ticker}
+      </div>
+      <div className={`right ${
+        stockQuoteData.d > 0 ? "green" :
+        stockQuoteData.d < 0 ? "red" : "gray"
+      }`}>
+        {stockQuoteData.d > 0 ? "+" : ""}{stockQuoteData.d} ({Math.abs(stockQuoteData.dp).toFixed(2)}%)
+      </div>
+    </div>
+  );
+}
+
+export default StockCard;
